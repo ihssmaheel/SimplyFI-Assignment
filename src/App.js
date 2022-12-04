76d4746fb81a6de1dd455f7e727a0de73743155f
Mohamed Ismail <111420634+ihssmaheel@users.noreply.github.com>
2022-12-04 19:39:44 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <div className="body flex-center">
+        <div className="container flex-center">
+          <div className="box flex-center">
+                  <div className="text-box">
+                      <p>HTML<span>&</span>CSS</p>
+                      <p>design and build websites</p>
+                  </div>
+          </div>
+          <div className="bar top"></div>
+          <div className="bar right"></div>
+          <div className="bar bottom"></div>
+          <div className="bar left"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
+export default App;
